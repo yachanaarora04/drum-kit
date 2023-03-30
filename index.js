620e5b13022939166dad5ad25dde3805b4b0358e
Yachana Arora <yachanaarora04@gmail.com>
2023-03-30 19:49:12 +0530

    Add files via upload
    files added HTML,CSS,JS

---
+++ index.js
@@ -0,0 +1,80 @@
+for(var i=0;i<document.querySelectorAll(".drum").length;i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var buttonInnerHTML=this.innerHTML;
+
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    }
+    );
+   
+}
+
+for(var i=0;i<document.querySelectorAll(".drum").length;i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("keydown",function(event) //here keydown is the eventlistener that will be exected 
+    //and perform the function on (event). once the event is detected then the function gets called. event listener helped in detection by listening the event.
+    //event has a property key.
+    {
+        makeSound(event.key); //.key will give the key value that is pressed on the keyboard.
+        buttonAnimation(event.key);
+    }
+    );
+}
+
+ 
+function makeSound(key)
+{
+    switch(key)
+    {
+            case "w":
+                var tom1 = new Audio('sounds/tom-1.mp3');
+                tom1.play();
+                break;
+
+            case "a":
+                var tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.play();
+                break;
+
+            case "s":
+                var tom3 = new Audio('sounds/tom-3.mp3');
+                tom3.play();
+                break;
+
+            case "d":
+                var tom4 = new Audio('sounds/tom-4.mp3');
+                tom4.play();
+                break;
+
+            case "j":
+                var crash = new Audio('sounds/crash.mp3');
+                crash.play();
+                break;
+
+            case "k":
+                var kickBass = new Audio('sounds/kick-bass.mp3');
+                kickBass.play();
+                break;
+
+            case "l":
+                var snare = new Audio('sounds/snare.mp3');
+                snare.play();
+                break;
+
+            default:console.log(buttonInnerHTML);
+        }
+
+}
+
+function buttonAnimation(currentKey)
+{
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function()
+    {
+        activeButton.classList.remove("pressed");
+    },100);
+
+}
